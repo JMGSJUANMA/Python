2b032f4b18d8f30ca7f882c1012a084e8b23b703
JMGARCIA82 <juanmanuelgarcia1982@gmail.com>
2025-01-12 13:01:06 +0100

    ImportandoModulosEnPythEjMathCalcularRaizCuadrada

---
+++ AplicaionesBasicas/ImportacionesEnPython.py
@@ -0,0 +1,12 @@
+# Importamos el módulo math
+import math
+
+x = int(input("Ingrese un número:\n"))
+# utilizamos la función sqrt del módulo math
+
+raiz_cuadrada = math.sqrt(x)
+# imprimimos el resultado
+print("La raíz cuadrada de", x, "es", raiz_cuadrada)
+
+# Ejecutar el código para obtener la raíz cuadrada de un número ingresado por el usuario.
+#
